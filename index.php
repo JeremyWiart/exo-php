cdd4d560ab2d1e2d4f30ffb6ebd73a9bef6eb5d9
JeremyWiart <44949951+JeremyWiart@users.noreply.github.com>
2024-11-22 14:42:58 +0100

    creation button,page info et redirection

--- index.php
+++ index.php
@@ -6,4 +6,13 @@ echo "adresse IP (Local) : ".$_SERVER['REMOTE_ADDR']."</br>";
 
 echo "Nom du serveur : ".$_SERVER['SERVER_NAME'];
 
+echo "<form action=\"#\" method=\"POST\">
+  <input name=\"info\" type=\"submit\" value=\"Page info\">
+</form>";
+
+if(isset($_POST['info'])){
+    header("location:http://localhost/info.php");
+
+}
+
 ?>
