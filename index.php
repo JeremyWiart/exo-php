122e428de655974a504e260529154370b09b9c1a
JeremyWiart <44949951+JeremyWiart@users.noreply.github.com>
2024-11-22 14:32:52 +0100

    exo_1 user agent ip and server name

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+echo "User Agent : ".$_SERVER['HTTP_USER_AGENT']."\n\n</br>";
+
+echo "adresse IP (Local) : ".$_SERVER['REMOTE_ADDR']."</br>";
+
+echo "Nom du serveur : ".$_SERVER['SERVER_NAME'];
+
+?>
